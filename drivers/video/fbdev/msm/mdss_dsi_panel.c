d11692d2c9dd039829b51d480984f31ed008b0aa
Yumi Yukimura <me.cafebabe@gmail.com>
2024-02-20 01:15:01 +0800

    msm: mdss: Print all mismatched regs as errors during ESD check by reg_read
    Change-Id: I0b3002d2a6c18b1d90c68f48ff55e1953ed4d165

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -1851,6 +1851,7 @@ static int mdss_dsi_truly_set_sleep_out(struct mdss_dsi_ctrl_pdata *ctrl)
 
 static bool mdss_dsi_cmp_panel_reg_v2(struct mdss_dsi_ctrl_pdata *ctrl)
 {
+	bool mismatched;
 	int i, j = 0;
 	int len = 0, *lenp;
 	int group = 0;
@@ -1868,18 +1869,20 @@ static bool mdss_dsi_cmp_panel_reg_v2(struct mdss_dsi_ctrl_pdata *ctrl)
 		len += lenp[i];
 
 	for (j = 0; j < ctrl->groups; ++j) {
+		mismatched = false;
 		for (i = 0; i < len; ++i) {
-			pr_debug("[%i] return:0x%x status:0x%x\n",
-				i, ctrl->return_buf[i],
-				(unsigned int)ctrl->status_value[group + i]);
 			MDSS_XLOG(ctrl->ndx, ctrl->return_buf[i],
 					ctrl->status_value[group + i]);
 			if (ctrl->return_buf[i] !=
-				ctrl->status_value[group + i])
-				break;
+				ctrl->status_value[group + i]) {
+				pr_err("%s: [%i][%i] return:0x%x status:0x%x\n",
+					__func__, j, i, ctrl->return_buf[i],
+					(unsigned int)ctrl->status_value[group + i]);
+				mismatched = true;
+			}
 		}
 
-		if (i == len)
+		if (i == len && !mismatched)
 			return true;
 		group += len;
 	}
