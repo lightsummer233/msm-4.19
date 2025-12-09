ed7a57291fade50957a95a2c45931412c2a7a32b
KanadeAlt <enashinonome264@gmail.com>
2025-12-09 05:18:21 +0000

    init: main: import FP Selection for xiaomi-ysl

--- init/main.c
+++ init/main.c
@@ -555,10 +555,16 @@ static void __init mm_init(void)
 	pti_init();
 }
 
+#ifdef CONFIG_MACH_XIAOMI_YSL
+int fpsensor=1;
+#endif
 asmlinkage __visible void __init start_kernel(void)
 {
 	char *command_line;
 	char *after_dashes;
+#ifdef CONFIG_MACH_XIAOMI_YSL
+	char *p=NULL;
+#endif
 
 	set_task_stack_end_magic(&init_task);
 	smp_setup_processor_id();
@@ -588,6 +594,13 @@ asmlinkage __visible void __init start_kernel(void)
 	page_alloc_init();
 
 	pr_notice("Kernel command line: %s\n", boot_command_line);
+#ifdef CONFIG_MACH_XIAOMI_YSL
+	p = strstr(boot_command_line, "androidboot.fpsensor=fpc");
+	if (p)
+		fpsensor = 1; // fpc1020
+	else
+		fpsensor = 2; // gf3208
+#endif
 	/* parameters may set static keys */
 	jump_label_init();
 	parse_early_param();
