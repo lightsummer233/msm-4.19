715acb7fd3feba79b9d9784b38abd95667bff97d
amemika <297562698+AmeMika@users.noreply.github.com>
2026-06-29 01:10:45 +0000

    techpack: camera-legacy: Don't enable on mido and markw
    * If Legacy (Marshmallow & Nougat) camera stack is enabled
    
    Change-Id: I45789c6f735fbea05c66e6fbfe40e56f21729389

--- techpack/camera-legacy/init.c
+++ techpack/camera-legacy/init.c
@@ -1,5 +1,8 @@
 #include <linux/module.h>
 #include <linux/printk.h>
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8953)
+#include <xiaomi-msm8953/mach.h>
+#endif
 
 /*
 extern bool camera_legacy_enable;
@@ -13,6 +16,14 @@ bool camera_legacy_enable = false;
 static int __init camera_legacy_init(void)
 {
 	camera_legacy_enable = true;
+#if IS_ENABLED(CONFIG_MSMB_CAMERA_LEGACY_M) || \
+    IS_ENABLED(CONFIG_MSMB_CAMERA_LEGACY_N)
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_MIDO) || IS_ENABLED(CONFIG_MACH_XIAOMI_MARKW)
+	if (xiaomi_msm8953_mach_get() == XIAOMI_MSM8953_MACH_MIDO ||
+		xiaomi_msm8953_mach_get() == XIAOMI_MSM8953_MACH_MARKW)
+		camera_legacy_enable = false;
+#endif
+#endif
 
 	if (camera_legacy_enable)
 		pr_info("Enable Legacy Camera Stack.\n");
